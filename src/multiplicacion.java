3aabedbe97890ce3126d148f2a48c117ca842fac
cococoquito <adiz98@hotmail.com>
2019-08-19 12:55:39 -0500

    Update multiplicacion.java

--- src/multiplicacion.java
+++ src/multiplicacion.java
@@ -1,10 +1,13 @@
 import javax.swing.JOptionPane;
-
+// no hay documentacion, porfa mire bien como se documenta las clases variables y metodo
+// por estandar POO siempre las clases deben empezar con minuscula (Division)
 public class multiplicacion 
 {
+	// este metodo debe retornar el resultado
 	public void multiplicar(double num1,double num2)
 	{
 		double resultado = num1*num2;
-		JOptionPane.showMessageDialog(null,"Su multiplicación es: "+resultado);
+		// esta clase debe tener la responsabilidad de hacer la * no de mostrar ningun mensaje
+		JOptionPane.showMessageDialog(null,"Su multiplicaciÃ³n es: "+resultado);
 	}
 }
