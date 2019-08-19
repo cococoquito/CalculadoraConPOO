c298085eba278950207f707cc2531c617b6726cb
cococoquito <adiz98@hotmail.com>
2019-08-19 13:10:45 -0500

    Update suma.java

--- src/suma.java
+++ src/suma.java
@@ -1,12 +1,15 @@
 import javax.swing.JOptionPane;
-
+// no hay documentacion, porfa mire bien como se documenta las clases variables y metodo
+// por estandar POO siempre las clases deben empezar con minuscula (Division)
 public class suma 
 {
+	// este metodo debe retornar el resultado
 	public void sumar(double numero1, double numero2)
 	{
-			
+		// no hay necesidad de crear una variable para sumar se puede retonrar directamente el resultado
 		double resultado;
 		resultado = numero1+numero2;
+		// esta clase debe tener la responsabilidad de hacer la + no de mostrar ningun mensaje
 		JOptionPane.showMessageDialog(null,"La suma es: "+resultado);
 		
 	}
